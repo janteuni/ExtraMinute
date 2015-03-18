8fdb5a12f9900354e2a42d184717398de38ba557
janteuni <janteuni@student.42.fr>
2015-03-18 23:05:51 +0100

    feat(search): change directory image

--- client/views/pro-search/pro-search.controller.js
+++ client/views/pro-search/pro-search.controller.js
@@ -10,59 +10,54 @@ angular.module('extraMinute')
         return new Array(num);
       },
       results: [
-        {
-          name: 'Annie Touhi',
-          img: 'assets/images/2.jpg',
-          stars: 3,
-          job: 'Sommelier'
-        }, {
+         {
           name: 'Thomas Dumant',
-          img: 'assets/images/2.jpg',
+          img: 'assets/images/faces/2.jpg',
           stars: 4,
           job: 'Service'
         }, {
           name: 'Alice Poulis',
-          img: 'assets/images/2.jpg',
+          img: 'assets/images/faces/3.jpg',
           stars: 4,
           job: 'Service'
         }, {
           name: 'Christine Moh',
-          img: 'assets/images/2.jpg',
+          img: 'assets/images/faces/4.jpg',
           stars: 2,
           job: 'Service'
         }, {
           name: 'Paul Luanard',
-          img: 'assets/images/2.jpg',
+          img: 'assets/images/faces/5.jpg',
           stars: 4,
           job: 'Sommelier'
         }, {
           name: 'Vincent chomier',
-          img: 'assets/images/2.jpg',
+          img: 'assets/images/faces/6.jpg',
           stars: 3,
           job: 'Commis'
         }, {
           name: 'Delphine Plusis',
-          img: 'assets/images/2.jpg',
+          img: 'assets/images/faces/7.jpg',
           stars: 3,
           job: 'Sommelier'
         }, {
           name: 'Marc Lotus',
-          img: 'assets/images/2.jpg',
+          img: 'assets/images/faces/11.jpg',
           stars: 2,
           job: 'Barman'
         }, {
           name: 'Gilles Devinc',
-          img: 'assets/images/2.jpg',
+          img: 'assets/images/faces/9.jpg',
           stars: 2,
           job: 'Service'
         }, {
           name: 'Charlotte Bauto',
-          img: 'assets/images/2.jpg',
+          img: 'assets/images/faces/10.jpg',
           stars: 2,
           job: 'Commis'
         }, {
-          name: 'Aline louan',
-          img: 'assets/images/2.jpg',
+          name: 'Alain louan',
+          img: 'assets/images/faces/1.jpg',
           stars: 4,
           job: 'Barman'
         }
