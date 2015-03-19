da9290d5d6d208589097ee92c0eefb3ab59d57c2
meriadec <meriadec.pillet@gmail.com>
2015-03-19 05:45:53 +0100

    style(login): beautify

--- client/views/login/login.controller.js
+++ client/views/login/login.controller.js
@@ -8,8 +8,10 @@ angular.module('extraMinute')
     var box = angular.element('.card.pad');
     var back = angular.element('.login-back');
     var login = angular.element('.login');
-    var loader = angular.element('.login-loader');
+    var loader = angular.element('.login-loader .hook');
     var header = angular.element('.login > header');
+    var demoExtra = angular.element('.mockup .btn:nth-child(1)');
+    var demoResto = angular.element('.mockup .btn:nth-child(2)');
 
     // animation
 
@@ -18,9 +20,13 @@ angular.module('extraMinute')
     t
       .set(box, { y: 250, opacity: 0 })
       .set(back, { opacity: 0, y: -30 })
-      .set(loader, { opacity: 0, y: 40 })
+      .set(loader, { opacity: 0, scale: 2 })
+      .set(demoExtra, { x: '-110%', opacity: 0 })
+      .set(demoResto, { x: '-110%', opacity: 0 })
       .to(box, .5, { y: 0, opacity: 1, ease: 'Back.easeOut' })
-      .to(back, .5, { y: 0, opacity: 1, ease: 'Back.easeOut', delay: -.1 });
+      .to(back, .5, { y: 0, opacity: 1, ease: 'Back.easeOut', delay: -.1 })
+      .to(demoExtra, 0.3, { opacity: 1, x: '0%', ease: 'Back.easeOut', delay: -0.5 })
+      .to(demoResto, 0.3, { opacity: 1, x: '0%', ease: 'Back.easeOut', delay: -0.45 });
 
     // vm
 
@@ -28,10 +34,12 @@ angular.module('extraMinute')
       login: function () {
         var t = new TimelineMax();
         t
+          .to(demoResto, 0.25, { x: '-110%', opacity: 0, ease: 'Back.easeIn' })
+          .to(demoExtra, 0.25, { x: '-110%', opacity: 0, delay: -0.15, ease: 'Back.easeIn' })
           .to(back, 0.25, { y: -30, opacity: 0 })
           .to(box, .5, { y: 30, opacity: 0, delay: -0.1 })
-          .to(loader, 0.5, { scale: 1, y: 0, opacity: 1, delay: -0.5, ease: 'Back.easeInOut' })
-          .to(loader, 0.25, { opacity: 0, y: 30, delay: 0.5, ease: 'Back.easeInOut' })
+          .to(loader, 0.25, { scale: 1, opacity: 1, delay: -0.5, ease: 'Back.easeOut' })
+          .to(loader, 0.25, { opacity: 0, y: 30, delay: 0.5, ease: 'Back.easeIn' })
           .to(header, 0.25, { opacity: 0, y: 30, delay: -0.25 })
           .addCallback(function () {
             $location.path('/extra/slots');
