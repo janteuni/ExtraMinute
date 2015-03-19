46b2520ca2f9a600e2adddf2de40c82f1dc86a39
janteuni <janteuni@student.42.fr>
2015-03-19 06:56:49 +0100

    feat(offers): list of offers in profile

--- client/views/pro-offers/pro-offers.controller.js
+++ client/views/pro-offers/pro-offers.controller.js
@@ -6,6 +6,54 @@ angular.module('extraMinute')
     var vm = this;
 
     angular.extend(vm, {
+      selectOffer: function() {
+        vm.offers[0].jobs[1].status = 'booked';
+        vm.offers[0].jobs.splice(2);
+      },
+      offers: [
+        {
+          date: '20 Mars - Diner',
+          status: 'available',
+          jobs: [
+            {
+              name: 'Cuisto',
+              extra: 'Luis Azealo',
+              status: 'booked'
+            },
+            {
+              name: 'Sommelier',
+              extra: 'Anne Poulis',
+              status: 'confirmed'
+            },
+            {
+              name: 'Sommelier',
+              extra: 'Vincent Tourah',
+              status: 'asked'
+            },
+            {
+              name: 'Sommelier',
+              extra: 'Marc Loupp',
+              status: 'asked'
+            }
+          ]
+        },
+        {
+          date: '21 Mars - Diner',
+          status: 'available',
+          jobs: [
+            {
+              name: 'Sommelier',
+              extra: 'Anne Poulis',
+              status: 'asked'
+            },
+            {
+              name: 'Sommelier',
+              extra: 'Paul Bichat',
+              status: 'asked'
+            }
+          ]
+        }
+      ]
     });
 
   });
