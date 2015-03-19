b2761aa54d661a21cd5f8d675d140c538038a055
meriadec <meriadec.pillet@gmail.com>
2015-03-19 04:04:13 +0100

    style(core): core feat

--- client/views/extra-slots/extra-slots.controller.js
+++ client/views/extra-slots/extra-slots.controller.js
@@ -5,8 +5,27 @@ angular.module('extraMinute')
 
     var vm = this;
 
+    var jours = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+
     angular.extend(vm, {
-      name: 'ExtraSlotsCtrl'
+
+      days: (function () {
+        var out = [];
+        var current = moment();
+        var last = moment(current).add(7, 'days');
+        while (!current.isSame(last)) {
+          out.push(moment(current));
+          current.add(1, 'days');
+        }
+        return out;
+      })(),
+
+      getLabel: function (day) {
+        if (vm.days.indexOf(day) === 0) { return 'Aujourd\'hui'; }
+        if (vm.days.indexOf(day) === 1) { return 'Demain'; }
+        return jours[day.toDate().getDay()];
+      }
+
     });
 
   });
